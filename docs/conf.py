60e5397c18a8f543d2b22f7252f4539a2276e136
Ethan Look-Potts <elookpotts@nvidia.com>
2025-11-21 14:11:31 -0500

    #71 - Add a root documentation page

--- docs/conf.py
+++ docs/conf.py
@@ -22,6 +22,10 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 sys.path.insert(0, os.path.abspath('.'))
 
+# Determine if we're building from a subdirectory or root
+_conf_dir = os.path.dirname(os.path.abspath(__file__))
+_is_subdir = os.path.basename(_conf_dir) in ['user_guide', 'deployment_guide']
+
 # -- Project information -----------------------------------------------------
 
 project = 'NVIDIA OSMO'
@@ -29,6 +33,9 @@ copyright = "2025 NVIDIA CORPORATION & AFFILIATES"
 author = "NVIDIA"
 
 # -- General configuration ---------------------------------------------------
+
+# Add templates directory
+templates_path = ['_templates']
 
 extensions = [
     # Standard extensions
@@ -81,8 +88,17 @@ exclude_patterns = [
     '**/*.in.rst',  # Ignore files that are embedded in other files
 ]
 
+# When building from root, exclude everything in subdirectories
+# The index files will still be parsed for TOC but won't build full pages
+if not _is_subdir:
+    exclude_patterns.extend([
+        'user_guide/**',
+        'deployment_guide/**',
+    ])
+
 suppress_warnings = [
-    'toc.excluded'
+    'toc.excluded',
+    'toc.not_readable',  # Suppress warnings about excluded toctree entries
 ]
 
 # -- Options for HTML output -------------------------------------------------
@@ -92,10 +108,24 @@ suppress_warnings = [
 #
 html_theme = "nvidia_sphinx_theme"
 
-html_title = 'OSMO User Guide'
+html_title = 'OSMO Documentation'
 html_show_sourcelink = False
-html_favicon = '../_static/osmo_favicon.png'
-html_logo = '../_static/nvidia-logo-horiz-rgb-wht-for-screen.png'
+
+if _is_subdir:
+    html_favicon = '../_static/osmo_favicon.png'
+    html_logo = '../_static/nvidia-logo-horiz-rgb-wht-for-screen.png'
+    html_static_path = ['../_static']
+    html_css_files_extra = []
+else:
+    html_favicon = '_static/osmo_favicon.png'
+    html_logo = '_static/nvidia-logo-horiz-rgb-wht-for-screen.png'
+    html_static_path = ['_static']
+    # Hide sidebar completely for root page
+    html_sidebars = {
+        "**": []
+    }
+    # Add custom CSS to hide sidebar and remove vertical bar
+    html_css_files_extra = ['css/root_page.css']
 
 html_theme_options = {
     "collapse_navigation": False,
@@ -109,10 +139,7 @@ html_extra_path_opts = {
     'follow_symlinks': True,
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
+# Note: html_static_path is set dynamically above based on build directory
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
@@ -122,6 +149,10 @@ html_css_files = [
     'mermaid_custom.css',
     'https://cdn.jsdelivr.net/npm/glightbox/dist/css/glightbox.min.css',
 ]
+
+# Add extra CSS files for root page
+if 'html_css_files_extra' in dir() and html_css_files_extra:
+    html_css_files.extend(html_css_files_extra)
 
 # JavaScript files to include in the HTML output
 # Files are loaded in the order they appear in this list
