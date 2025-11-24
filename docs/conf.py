7c9234e31be438d38056411adeb241bfb120836e
Ryan Li <ryali@nvidia.com>
2025-11-23 19:37:29 -0800

    Verified check

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ spelling_word_list_filename = '../spelling_wordlist.txt'
 linkcheck_ignore = [
     'https://github.com/settings/tokens/new',
     'https://osmo-example-url.com*',
+    'https://github.com/NVIDIA/Isaac-GR00T?tab=readme-ov-file#prerequisites',
 ]
 
 # Copybutton
