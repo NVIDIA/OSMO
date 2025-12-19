34784452921f58a8113d499ecbe69ebd307b1881
Fernando L <fernandol@nvidia.com>
2025-12-19 14:16:11 -0800

    Support Credential-less Data Operations in Data Access Layer (#159)
    * Move data.constants into data.storage.constants
    
    * Add data.storage.credentials
    
    * Update DAL to handle credential-less operations
    
    * Support different credential strategies
    
    * Update dataset to be keyless
    
    * Fix build/linter issues for keyless data creds
    
    * Revert formatting
    
    * Update credential resolution
    
    * Linter
    
    * Switch to a cached property for resolved_data_credential
    
    * Fix lint
    
    * Interop with existing credential objects in db
    
    * Address feedback
    
    * Adjust validate_data logic
    
    * Lint
    
    * Address comments
    
    * Make workflow validate_data more intuitive
    
    * Refine resolved_data_credential
    
    * Clean up

--- src/utils/job/workflow.py
+++ src/utils/job/workflow.py
@@ -647,15 +647,22 @@ class WorkflowSpec(pydantic.BaseModel, extra=pydantic.Extra.forbid):
 
             data_cred = task.fetch_creds(user, user_creds, bucket_info.uri)
 
-            # Get if user has access to READ or WRITE
-            if is_input and bucket_info.uri not in seen_uri_input:
-                bucket_info.data_auth(data_cred['access_key_id'], data_cred['access_key'],
-                                      data_cred['region'], storage.AccessType.READ)
-                seen_uri_input.add(bucket_info.uri)
-            if not is_input and bucket_info.uri not in seen_uri_output:
-                bucket_info.data_auth(data_cred['access_key_id'], data_cred['access_key'],
-                                      data_cred['region'], storage.AccessType.WRITE)
-                seen_uri_output.add(bucket_info.uri)
+            if data_cred is None:
+                # User does not have any credentials, check if the backend
+                # supports environment authentication
+                if not bucket_info.supports_environment_auth:
+                    raise osmo_errors.OSMOCredentialError(
+                        f'Could not validate access to {bucket_info.uri} for user {user}.')
+            else:
+                # Check if user credentials have access to READ
+                if is_input and bucket_info.uri not in seen_uri_input:
+                    bucket_info.data_auth(data_cred, storage.AccessType.READ)
+                    seen_uri_input.add(bucket_info.uri)
+
+                # Check if user credentials have access to WRITE
+                if not is_input and bucket_info.uri not in seen_uri_output:
+                    bucket_info.data_auth(data_cred, storage.AccessType.WRITE)
+                    seen_uri_output.add(bucket_info.uri)
 
         for input_data_spec in group_task.inputs:
             _validate_input_output(input_data_spec, True)
