6be5964bb8bb8583d43cbc5f5443ff562e97215c
Fernando Luo <fernandol@nvidia.com>
2025-12-25 13:25:12 -0800

    Format

--- ui-next/src/app/(dashboard)/resources/page.tsx
+++ ui-next/src/app/(dashboard)/resources/page.tsx
@@ -131,7 +131,6 @@ export default function ResourcesPage() {
                       onChange={toggleResourceType}
                     />
                   )}
-
                 </FilterBar>
               }
               summaryContent={
