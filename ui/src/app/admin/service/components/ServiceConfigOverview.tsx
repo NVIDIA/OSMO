791021a107c195bc5ab7615ac2361fa11ad28984
Pat Clark <patc@nvidia.com>
2026-01-15 09:35:37 -0800

    Diff component

--- ui/src/app/admin/service/components/ServiceConfigOverview.tsx
+++ ui/src/app/admin/service/components/ServiceConfigOverview.tsx
@@ -13,12 +13,12 @@
 //limitations under the License.
 //
 //SPDX-License-Identifier: Apache-2.0
-import { useCallback } from "react";
-
 import { Colors, Tag } from "~/components/Tag";
 import { type ServiceConfig } from "~/models/config/service-config";
 
 const highlightClass = "bg-yellow-100";
+
+type GetValue = (config: ServiceConfig) => unknown;
 
 export const ServiceConfigOverview = ({
   serviceConfig,
@@ -29,8 +29,16 @@ export const ServiceConfigOverview = ({
   previousConfig?: ServiceConfig;
   isShowingJSON: boolean;
 }) => {
-  const isChanged = useCallback(
-    (getValue: (config: ServiceConfig) => unknown): boolean => {
+  const Diff = ({
+    getValue,
+    children,
+    className,
+  }: {
+    getValue: GetValue;
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    const isChanged = (getValue: GetValue): boolean => {
       if (!previousConfig) {
         return false;
       }
@@ -42,13 +50,22 @@ export const ServiceConfigOverview = ({
         return true;
       }
       return JSON.stringify(current) !== JSON.stringify(previous);
-    },
-    [previousConfig, serviceConfig],
-  );
+    };
+
+    return (
+      <div
+        className={`inline-flex flex-row flex-wrap gap-1 ${className} ${isChanged(getValue) ? highlightClass : ""}`}
+        aria-live={isChanged(getValue) ? "polite" : undefined}
+      >
+        {children}
+      </div>
+    );
+  };
 
   if (isShowingJSON) {
     return <pre className="p-global text-sm font-mono">{JSON.stringify(serviceConfig, null, 2)}</pre>;
   }
+
   return (
     <div className="flex flex-col gap-global p-global w-full h-full">
       <dl
@@ -57,39 +74,27 @@ export const ServiceConfigOverview = ({
       >
         <dt>Service Base URL</dt>
         <dd>
-          <span className={isChanged((config) => config.service_base_url) ? highlightClass : ""}>
-            {serviceConfig.service_base_url}
-          </span>
+          <Diff getValue={(config) => config.service_base_url}>{serviceConfig.service_base_url}</Diff>
         </dd>
         <dt>Max Pod Restart Limit</dt>
         <dd>
-          <span className={isChanged((config) => config.max_pod_restart_limit) ? highlightClass : ""}>
-            {serviceConfig.max_pod_restart_limit}
-          </span>
+          <Diff getValue={(config) => config.max_pod_restart_limit}>{serviceConfig.max_pod_restart_limit}</Diff>
         </dd>
         <dt>Agent Queue Size</dt>
         <dd>
-          <span className={isChanged((config) => config.agent_queue_size) ? highlightClass : ""}>
-            {serviceConfig.agent_queue_size}
-          </span>
+          <Diff getValue={(config) => config.agent_queue_size}>{serviceConfig.agent_queue_size}</Diff>
         </dd>
         <dt>Issuer</dt>
         <dd>
-          <span className={isChanged((config) => config.service_auth.issuer) ? highlightClass : ""}>
-            {serviceConfig.service_auth.issuer}
-          </span>
+          <Diff getValue={(config) => config.service_auth.issuer}>{serviceConfig.service_auth.issuer}</Diff>
         </dd>
         <dt>Audience</dt>
         <dd>
-          <span className={isChanged((config) => config.service_auth.audience) ? highlightClass : ""}>
-            {serviceConfig.service_auth.audience}
-          </span>
+          <Diff getValue={(config) => config.service_auth.audience}>{serviceConfig.service_auth.audience}</Diff>
         </dd>
         <dt>User Roles</dt>
         <dd>
-          <div
-            className={`flex flex-wrap gap-1 ${isChanged((config) => config.service_auth.user_roles) ? highlightClass : ""}`}
-          >
+          <Diff getValue={(config) => config.service_auth.user_roles}>
             {serviceConfig.service_auth.user_roles.map((role) => (
               <Tag
                 color={Colors.tag}
@@ -99,13 +104,11 @@ export const ServiceConfigOverview = ({
                 {role}
               </Tag>
             ))}
-          </div>
+          </Diff>
         </dd>
         <dt>Ctrl Roles</dt>
         <dd>
-          <div
-            className={`flex flex-wrap gap-1 ${isChanged((config) => config.service_auth.ctrl_roles) ? highlightClass : ""}`}
-          >
+          <Diff getValue={(config) => config.service_auth.ctrl_roles}>
             {serviceConfig.service_auth.ctrl_roles.map((role) => (
               <Tag
                 color={Colors.pool}
@@ -115,90 +118,86 @@ export const ServiceConfigOverview = ({
                 {role}
               </Tag>
             ))}
-          </div>
+          </Diff>
         </dd>
         <dt>Max Token Duration</dt>
         <dd>
-          <span className={isChanged((config) => config.service_auth.max_token_duration) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.service_auth.max_token_duration}>
             {serviceConfig.service_auth.max_token_duration}
-          </span>
+          </Diff>
         </dd>
         <dt>CLI Latest Version</dt>
         <dd>
-          <span className={isChanged((config) => config.cli_config.latest_version) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.cli_config.latest_version}>
             {serviceConfig.cli_config.latest_version ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>CLI Min Supported Version</dt>
         <dd>
-          <span className={isChanged((config) => config.cli_config.min_supported_version) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.cli_config.min_supported_version}>
             {serviceConfig.cli_config.min_supported_version ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Device Client ID</dt>
         <dd>
-          <span
-            className={isChanged((config) => config.service_auth.login_info.device_client_id) ? highlightClass : ""}
-          >
+          <Diff getValue={(config) => config.service_auth.login_info.device_client_id}>
             {serviceConfig.service_auth.login_info.device_client_id ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Browser Client ID</dt>
         <dd>
-          <span
-            className={isChanged((config) => config.service_auth.login_info.browser_client_id) ? highlightClass : ""}
-          >
+          <Diff getValue={(config) => config.service_auth.login_info.browser_client_id}>
             {serviceConfig.service_auth.login_info.browser_client_id ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Keys</dt>
         <dd className="lg:col-span-3">
-          <ul className={`list-none ${isChanged((config) => config.service_auth.keys) ? highlightClass : ""}`}>
-            {Object.entries(serviceConfig.service_auth.keys).map(([key, _value]) => {
-              return (
-                <li
-                  className="flex flex-row gap-1"
-                  key={key}
-                >
-                  {key}
-                  {key === serviceConfig.service_auth.active_key ? (
-                    <Tag
-                      className="inline-block"
-                      color={Colors.tag}
-                    >
-                      Active
-                    </Tag>
-                  ) : undefined}
-                </li>
-              );
-            })}
-          </ul>
+          <Diff getValue={(config) => config.service_auth.keys}>
+            <ul className="list-none">
+              {Object.entries(serviceConfig.service_auth.keys).map(([key, _value]) => {
+                return (
+                  <li
+                    className="flex flex-row gap-1"
+                    key={key}
+                  >
+                    {key}
+                    {key === serviceConfig.service_auth.active_key ? (
+                      <Tag
+                        className="inline-block"
+                        color={Colors.tag}
+                      >
+                        Active
+                      </Tag>
+                    ) : undefined}
+                  </li>
+                );
+              })}
+            </ul>
+          </Diff>
         </dd>
         <dt>Device Endpoint</dt>
         <dd className="lg:col-span-3">
-          <span className={isChanged((config) => config.service_auth.login_info.device_endpoint) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.service_auth.login_info.device_endpoint}>
             {serviceConfig.service_auth.login_info.device_endpoint ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Browser Endpoint</dt>
         <dd className="lg:col-span-3">
-          <span
-            className={isChanged((config) => config.service_auth.login_info.browser_endpoint) ? highlightClass : ""}
-          >
+          <Diff getValue={(config) => config.service_auth.login_info.browser_endpoint}>
             {serviceConfig.service_auth.login_info.browser_endpoint ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Token Endpoint</dt>
         <dd className="lg:col-span-3">
-          <span className={isChanged((config) => config.service_auth.login_info.token_endpoint) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.service_auth.login_info.token_endpoint}>
             {serviceConfig.service_auth.login_info.token_endpoint ?? "-"}
-          </span>
+          </Diff>
         </dd>
         <dt>Logout Endpoint</dt>
         <dd className="lg:col-span-3">
-          <span className={isChanged((config) => config.service_auth.login_info.logout_endpoint) ? highlightClass : ""}>
+          <Diff getValue={(config) => config.service_auth.login_info.logout_endpoint}>
             {serviceConfig.service_auth.login_info.logout_endpoint ?? "-"}
-          </span>
+          </Diff>
         </dd>
       </dl>
     </div>
