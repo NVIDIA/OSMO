9c223d254dab0f2aa381519b638dd920fd8419e0
ecolternv <ecolter@nvidia.com>
2026-02-09 16:33:06 -0500

    #354 - Implement Topology Aware Scheduling (#368)
    * Remove auth router in agent service (#371)
    
    * add envoy lua filter to refresh id_token (#388)
    
    * add envoy lua filter to for id_token refresh
    
    * add filter to service and router
    
    * add validate token
    
    * only remove auth cookies
    
    * allow user configuration
    
    * Revert "Remove auth router in agent service (#371)" (#390)
    
    This reverts commit fac90f2b799ee9d11b77b3e8e81144abc7b8f4cd.
    
    * remove router refresh filter (#393)
    
    * Changes round 1
    
    * Fix database definitions
    
    * Add some fixes
    
    * Fix how subpools are created
    
    * Fix some minor issues after reveiwing code
    
    * Fix copyrights being too long
    
    * Refactor algorithm and code
    
    * Implement changes from gitlab comments
    
    * Simplify topology
    
    * Some fixes
    
    * Refactor to make more efficent
    
    * Fix typing error
    
    * Fix linting errors
    
    ---------
    
    Co-authored-by: RyaliNvidia <ryali@nvidia.com>
    Co-authored-by: Vivian Pan <vivianp@nvidia.com>

--- src/utils/job/backend_job_defs.py
+++ src/utils/job/backend_job_defs.py
@@ -1,5 +1,5 @@
 """
-SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.  # pylint: disable=line-too-long
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@ limitations under the License.
 SPDX-License-Identifier: Apache-2.0
 """
 
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Union
 
 import pydantic
 
@@ -70,15 +70,25 @@ class BackendCleanupGroupMixin(pydantic.BaseModel):
 
 class BackendSynchronizeQueuesMixin(pydantic.BaseModel):
     """
-    Reconciles the queues in the backend with the provided list of queues.
-    - Any queues that match the cleanup_spec but are not in the k8s_resources list will be deleted
-    - Any queues in both the cleanup_spec and k8s_resources list will be updated with the new spec
-    - Any queues in the k8s_resources list that do not match the cleanup_spec will be created
+    Reconciles scheduler K8s objects (queues, topologies, etc.) in the backend
+    with the provided list.
+    - Objects matching cleanup_specs but not in k8s_resources will be deleted
+    - Objects in both cleanup_specs and k8s_resources will be updated (or recreated if immutable)
+    - Objects in k8s_resources not matching cleanup_specs will be created
+
+    NOTE: cleanup_specs Union type can be removed after the next release. This is for
+    backwards compatibility with existing BackendSynchronizeQueuesMixin jobs that might
+    be enqueued when OSMO is redeployed with a new version.
     """
-    # Search for queues to cleanup using this spec
-    cleanup_spec: BackendCleanupSpec
-    # The k8s specs for the queues to create in the backend
+    # Search for objects using these specs (one per object type)
+    cleanup_specs: Union[List[BackendCleanupSpec], BackendCleanupSpec]
+    # The k8s specs for all objects to create/update in the backend
+    # Can contain mixed types (Queues, Topologies, etc.)
     k8s_resources: List[Dict]
+    # List of K8s kinds that have immutable fields and should be deleted/recreated
+    # instead of updated (e.g., ['Topology'] for kai.scheduler Topology CRD)
+    # Defaults to empty list for backwards compatibility
+    immutable_kinds: List[str] = []
 
 
 class BackendSynchronizeBackendTestMixin(pydantic.BaseModel):
