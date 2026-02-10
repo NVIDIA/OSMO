38728ea68d2c76694e7888bc967934ecaa65b948
Ethan Yu <ethany@nvidia.com>
2026-02-10 15:00:13 -0800

    add copyright

--- workflows/nut_pouring/nutpour_gr1t2_base_env_cfg.py
+++ workflows/nut_pouring/nutpour_gr1t2_base_env_cfg.py
@@ -1,7 +1,20 @@
-# Copyright (c) 2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
-# All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
+"""
+SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+SPDX-License-Identifier: Apache-2.0
+"""
 
 import tempfile
 import torch
@@ -223,7 +236,7 @@ class ObservationsCfg:
 
         robot_pov_cam_depth = ObsTerm(
             func=mdp.image,
-            params={"sensor_cfg": SceneEntityCfg("robot_pov_cam"), 
+            params={"sensor_cfg": SceneEntityCfg("robot_pov_cam"),
                     "data_type": "distance_to_image_plane",
                     "normalize": True,"normalize": False},
         )
