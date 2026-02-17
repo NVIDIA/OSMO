244493c7436ae2a36e6b8c74745dcb452f38a38e
Ryan Li <ryali@nvidia.com>
2026-02-17 11:14:22 -0800

    Modify profile list api to show role info

--- src/lib/utils/login.py
+++ src/lib/utils/login.py
@@ -1,5 +1,5 @@
 """
-SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -36,6 +36,7 @@ OSMO_AUTH_HEADER = 'x-osmo-auth'
 # If developer mode, the header to pass the osmo user in
 OSMO_USER_HEADER = 'x-osmo-user'
 OSMO_USER_ROLES = 'x-osmo-roles'
+OSMO_TOKEN_NAME_HEADER = 'x-osmo-token-name'
 # Don't use a token that will expire within the next N seconds
 EXPIRE_WINDOW = 3
 TIMEOUT = 60
