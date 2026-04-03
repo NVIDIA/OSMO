7672288e7cf2fa6826db9e84f01d5a8b1ab0be92
Keita Watanabe <keitaw09@gmail.com>
2026-04-03 00:06:50 +0000

    fix: support HEAD method on file-content endpoint for preview preflight
    The file preview panel sends a HEAD request before GET to check
    content-type and access. FastAPI's @router.get does not handle HEAD,
    returning 405 Method Not Allowed. Changed to @router.api_route with
    both GET and HEAD methods.

--- src/service/core/data/data_service.py
+++ src/service/core/data/data_service.py
@@ -1035,7 +1035,7 @@ def get_manifest(
     return json.loads(manifest_content)
 
 
-@router.get('/{bucket}/dataset/{name}/file-content')
+@router.api_route('/{bucket}/dataset/{name}/file-content', methods=['GET', 'HEAD'])
 def get_file_content(
     bucket: objects.DatasetPattern,
     name: objects.DatasetPattern,
